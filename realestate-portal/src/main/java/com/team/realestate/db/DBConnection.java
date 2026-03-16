05479eb577f6394b9b601ab0f5d7e910ad833da6
KRISH <krishpanasara9265@gmail.com>
2026-03-16 21:04:45 +0530

    Create DBConnection.java

---
+++ realestate-portal/src/main/java/com/team/realestate/db/DBConnection.java
@@ -0,0 +1,28 @@
+package com.team.realestate.db;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+public class DBConnection {
+
+	private static final String URL = "jdbc:mysql://localhost:3306/real_estate_db";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    public static Connection getConnection() {
+
+        Connection conn = null;
+
+        try {
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return conn;
+    }
+}
